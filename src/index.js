80582d459f611d04fd2b097cd76b1197dcc39301
Florent <florent.sylejmani@student.uni-pr.edu>
2025-10-30 14:20:42 +0100

    pizza-menu-final

--- src/index.js
+++ src/index.js
@@ -57,27 +57,67 @@ function App() {
 }
 
 function Header() {
-  
-  
   const style = {};
   return (
     <header className="header">
-      <h1 style={style}>
-        Fast React Pizza Co.
-      </h1>
+      <h1 style={style}>Fast React Pizza Co.</h1>
     </header>
   );
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas=[];
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+
+      {numPizzas > 0 ? (
+        <React.Fragment>
+          <p>Delicious pizzas made with fresh ingredients</p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObject={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>
+      ) : (
+        <p>We're still working on our menu. Please come back later :</p>
+      )}
+
+      {/* <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      /> */}
+      {/* <Flori name='Florent'/>
+      <Flori name='Alice'/>
+      <Flori name='Jona'/> */}
     </main>
+  );
+}
+
+function Pizza({ pizzaObject }) {
+  // console.log(props)
+  // if (pizzaObject.soldOut) return null;
+  return (
+    <li className={`pizza ${pizzaObject.soldOut ? "sold-out":""}`}>
+      <img src={pizzaObject.photoName} alt={pizzaObject.name} />
+      <div>
+        <h3>{pizzaObject.name}</h3>
+        <p>{pizzaObject.ingredients}</p>
+        <span>{pizzaObject.soldOut ? "Sold Out" : pizzaObject.price}</span>
+      </div>
+    </li>
   );
 }
 
@@ -94,20 +134,25 @@ function Footer() {
   // else alert("Sorry, We are closed");
 
   return (
-    <footer className="footer">{new Date().toLocaleTimeString()} - We are currently open!</footer>
+    <footer className="footer">
+      {isOpen ? (
+        <div className="order">
+          <p>We're open</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>We're closed</p>
+      )}
+    </footer>
   );
   // return React.createElement('footer', null, 'We are currently open!')
 }
 
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
+// function Flori(props){
+//   return <div>
+//     <h1>Hello im {props.name}</h1>
+//   </div>
+// }
 
 //React 18 way
 const root = ReactDOM.createRoot(document.getElementById("root"));
